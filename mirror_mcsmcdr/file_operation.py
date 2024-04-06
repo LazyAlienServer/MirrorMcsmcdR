e591eb0c28016d1ed4e3169f9aae6dae6471594f
tanh丶桁 <100672377+tanhHeng@users.noreply.github.com>
2024-04-06 16:17:09 +0800

    Add files via upload

--- mirror_mcsmcdr/file_operation.py
+++ mirror_mcsmcdr/file_operation.py
@@ -6,8 +6,11 @@ from mirror_mcsmcdr.constants import TITLE
 class WorldSync:
     
 
-    def __init__(self, world: list, source: str, target: str, ignore_files: list, concurrency: int) -> None:
-        self.world, self.source, self.target, self.ignore_files, self.concurrency = world, os.path.normpath(source), os.path.normpath(target), ignore_files, concurrency
+    def __init__(self, world: list, source: str, target: str, ignore_inexistent_target_path: bool, concurrency: int, ignore_files: list) -> None:
+        self.world, self.source, self.target = world, os.path.normpath(source), target
+        self.ignore_inexistent_target_path = ignore_inexistent_target_path
+        self.concurrency = concurrency
+        self.ignore_files = ignore_files
     
 
     def _get_md5(self, filename):
@@ -15,7 +18,7 @@ class WorldSync:
         with open(filename, "rb") as file:
             data = file.read(1000000) # 1MB
             m.update(data)
-            while data: # python has no "do while" !!! :-(
+            while data: # python doesn't have "do while" !!!  :-(
                 data = file.read(1000000)
                 m.update(data)
             file.close()
@@ -39,21 +42,33 @@ class WorldSync:
     def sync(self):
 
         changed_files_count = 0
+        paths_notfound = []
 
-        for single_world in self.world:
+        target_paths = [self.target] if type(self.target) == str else self.target
 
-            src_path = os.path.join(self.source, single_world)
-            dst_path = os.path.join(self.target, single_world)
+        for target_path in target_paths:
+            target_path = os.path.normpath(target_path)
 
-            src_files = [os.path.join(path, file_name)[len(src_path)+1:] for path, dir_lst, file_lst in os.walk(src_path) for file_name in file_lst]
-            dst_files = [os.path.join(path, file_name)[len(dst_path)+1:] for path, dir_lst, file_lst in os.walk(dst_path) for file_name in file_lst]
-            
-            for filename in set(dst_files) - set(src_files):
-                os.remove(os.path.join(dst_path, filename))
+            if not os.path.exists(target_path):
+                if not self.ignore_inexistent_target_path:
+                    paths_notfound.append(target_path)
+                else:
+                    os.makedirs(target_path)
 
-            with ThreadPoolExecutor(max_workers=self.concurrency) as t:
-                tasklist = [t.submit(lambda filename: self._copyfile_task(filename, src_path, dst_path), filename) for filename in set(src_files) & set(dst_files)]
-                wait(tasklist)
-                changed_files_count += sum([task.result() for task in tasklist])
+            for single_world in self.world:
+
+                src_path = os.path.join(self.source, single_world)
+                dst_path = os.path.join(target_path, single_world)
+
+                src_files = [os.path.join(path, file_name)[len(src_path)+1:] for path, dir_lst, file_lst in os.walk(src_path) for file_name in file_lst]
+                dst_files = [os.path.join(path, file_name)[len(dst_path)+1:] for path, dir_lst, file_lst in os.walk(dst_path) for file_name in file_lst]
+                
+                for filename in set(dst_files) - set(src_files):
+                    os.remove(os.path.join(dst_path, filename))
+
+                with ThreadPoolExecutor(max_workers=self.concurrency) as t:
+                    tasklist = [t.submit(lambda filename: self._copyfile_task(filename, src_path, dst_path), filename) for filename in set(src_files) & set(dst_files)]
+                    wait(tasklist)
+                    changed_files_count += sum([task.result() for task in tasklist])
         
-        return changed_files_count
+        return changed_files_count, paths_notfound
--- mirror_mcsmcdr/mirror_manager.py
+++ mirror_mcsmcdr/mirror_manager.py
@@ -1,8 +1,9 @@
-from mcdreforged.api.all import PluginServerInterface, CommandSource, CommandContext, Info, new_thread
+from mcdreforged.api.all import PluginServerInterface, CommandSource, CommandContext, Info, SimpleCommandBuilder, event_listener, new_thread
 from mirror_mcsmcdr.constants import DEFAULT_CONFIG, REPLY_TITLE, TITLE, VERSION
 from mirror_mcsmcdr.mcsm_api import MCSManagerApi, MCSManagerApiError
 from mirror_mcsmcdr.file_operation import WorldSync
 from threading import Event
+from copy import deepcopy
 import time, json, re
  
  
@@ -18,41 +19,91 @@ def catch_api_error(func):
     return wrapper
 
 
-class MirrorManager:
-
+class MultiMirrorManager: # The manager at large which manage multi single mirror server manager
 
     def __init__(self, server: PluginServerInterface) -> None:
-        global SERVER_NAME
+        try:
+            config = server.load_config_simple()
+        except:
+            config = server.load_config_simple(default_config=DEFAULT_CONFIG)
+
+        default_conf = config[list(config.keys())[0]]
+        self.managers = {}
+        for tag, single_conf in config.items():
+            server.logger.info(f"{tag}正在注册")
+            single_conf = self._conf_update(default_conf, single_conf)
+            single_manager = MirrorManager(server, single_conf, tag)
+            server.register_event_listener('mcdr.general_info', single_manager.on_info)
+            self.managers[tag] = single_manager
+    
+
+    def _conf_update(self, default_conf: dict, new_conf: dict):
+        default_conf = deepcopy(default_conf)
+        for key, value in new_conf.items():
+            if type(value) == dict:
+                default_conf[key] =  self._conf_update(default_conf[key], new_conf[key])
+            elif default_conf[key] != value:
+                default_conf[key] = value
+        return default_conf
+        
+        
+class MirrorManager: # The single mirror server manager which manage a specific mirror server
+
+
+    def __init__(self, server: PluginServerInterface, config: dict, tag="default") -> None:
         self.server = server
-        self.config = server.load_config_simple(default_config=DEFAULT_CONFIG)
-        if None in self.config["MCSManager"].values():
-            server.logger.warn("请设置配置文件")
+
+        server.logger.info(f"{tag}已加载")
+        command_prefix = tag
+        # check config
+        self.config = config
+        if None in self.config["mcsm"].values():
+            server.logger.warn(f"请设置配置文件({command_prefix})")
+            server.say(f"§c请设置配置文件§7({command_prefix})")
+            self.config = None
             return
-        self.mcsm_api = MCSManagerApi(**self.config["MCSManager"])
+        
+        # register api and file operation method
+        self.mcsm_api = MCSManagerApi(**self.config["mcsm"])
         self.world_sync = WorldSync(**self.config["sync"])
+
+        # set constants
         self.permission = self.config["command"]["permission"]
-        SERVER_NAME = self.config["display"]["server_name"]
+        self.server_name = self.config["display"]["server_name"]
+
+        # init flag
         self.sync_flag = False
         self.save_world_wait = Event()
 
-        command_prefix = self.config["command"]["prefix"]
+        # help info
         self.help_msg = [
             {"text":""},
             {"text":f"§7----- §e{TITLE} §b{VERSION} §7-----§f\n"},
             {"text":f"§7{command_prefix} help §f","clickEvent":{"action":"run_command","value":f"{command_prefix} help"}},
             {"text":"显示此帮助信息\n"},
             {"text":f"§7{command_prefix} status §f","clickEvent":{"action":"run_command","value":f"{command_prefix} status"}},
-            {"text":f"查看{SERVER_NAME}状态\n"},
+            {"text":f"查看{self.server_name}状态\n"},
             {"text":f"§7{command_prefix} start §f","clickEvent":{"action":"run_command","value":f"{command_prefix} start"}},
-            {"text":f"启动{SERVER_NAME}\n"},
+            {"text":f"启动{self.server_name}\n"},
             {"text":f"§7{command_prefix} stop §f","clickEvent":{"action":"run_command","value":f"{command_prefix} stop"}},
-            {"text":f"停止{SERVER_NAME}\n"},
+            {"text":f"停止{self.server_name}\n"},
             {"text":f"§7{command_prefix} kill §f","clickEvent":{"action":"run_command","value":f"{command_prefix} kill"}},
-            {"text":f"强制终止{SERVER_NAME}\n"},
+            {"text":f"强制终止{self.server_name}\n"},
             {"text":f"§7{command_prefix} sync §f","clickEvent":{"action":"run_command","value":f"{command_prefix} sync"}},
-            {"text":f"同步{SERVER_NAME}"},
+            {"text":f"同步{self.server_name}"},
         ]
-        self.command_prefix = command_prefix
+
+        # register mcdr command
+        builder = SimpleCommandBuilder()
+        builder.command(f"{command_prefix}", self.help)
+        builder.command(f"{command_prefix} help", self.help)
+        builder.command(f"{command_prefix} status", self.status)
+        builder.command(f"{command_prefix} start", self.start)
+        builder.command(f"{command_prefix} stop", self.stop)
+        builder.command(f"{command_prefix} kill", self.kill)
+        builder.command(f"{command_prefix} sync", self.sync)
+        builder.command(f"{command_prefix} sync", self.sync)
+        builder.register(server)
 
 
     def broadcast(self, text):
@@ -70,11 +121,11 @@ class MirrorManager:
     @catch_api_error
     def _execute(self, source: CommandSource, command: str, failed_prompt: dict, succeeded_prompt: dict): # <failed_prompt> & <succeeded_prompt> : {status_code: "prompt"}
         if not source.has_permission_higher_than(self.permission[command]):
-            source.reply(f"{REPLY_TITLE} §c权限不足")
+            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c权限不足")
             return
         status_code = self.mcsm_api.status()
         if status_code in failed_prompt.keys():
-            source.reply(f"{REPLY_TITLE} §c失败: "+failed_prompt[status_code])
+            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c失败: "+failed_prompt[status_code])
         elif status_code in succeeded_prompt.keys():
             rep = eval(f"self.mcsm_api.{command}()")
             self.server.logger.info(rep)
@@ -84,42 +135,42 @@ class MirrorManager:
     @catch_api_error
     def status(self, source: CommandSource, context: CommandContext):
         if not source.has_permission_higher_than(self.permission["status"]):
-            source.reply(f"{REPLY_TITLE} §c权限不足")
+            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c权限不足")
             return
-        source.reply(f"{REPLY_TITLE} : {SERVER_NAME}{self.mcsm_api.status_to_text[self.mcsm_api.status()]}")
+        source.reply(f"{REPLY_TITLE} §7({self.server_name})§b{self.server_name}§f{self.mcsm_api.status_to_text[self.mcsm_api.status()]}")
 
 
     def start(self, source: CommandSource, context: CommandContext):
         self._execute(
             source,
             "start",
-            {-1: f"{SERVER_NAME}状态未知, 请回报管理员",
-             1: f"{SERVER_NAME}正在停止, 请等待{SERVER_NAME}停止后重新启动",
-             2: f"{SERVER_NAME}正在启动, 请稍等",
-             3: f"{SERVER_NAME}正在运行"},
-            {0: f"正在启动{SERVER_NAME}..."})
+            {-1: f"{self.server_name}状态未知, 请回报管理员",
+             1: f"{self.server_name}正在停止, 请等待{self.server_name}停止后重新启动",
+             2: f"{self.server_name}正在启动, 请稍等",
+             3: f"{self.server_name}正在运行"},
+            {0: f"正在启动{self.server_name}..."})
     
 
     def stop(self, source: CommandSource, context: CommandContext):
         self._execute(
             source,
             "stop",
-            {-1: f"{SERVER_NAME}状态未知, 请回报管理员",
-             0: f"{SERVER_NAME}已停止",
-             1: f"{SERVER_NAME}正在停止, 请稍等",
-             2: f"{SERVER_NAME}正在启动, 请等待{SERVER_NAME}启动后重新停止",},
-            {3: f"正在关闭{SERVER_NAME}..."})
+            {-1: f"{self.server_name}状态未知, 请回报管理员",
+             0: f"{self.server_name}已停止",
+             1: f"{self.server_name}正在停止, 请稍等",
+             2: f"{self.server_name}正在启动, 请等待{self.server_name}启动后重新停止",},
+            {3: f"正在关闭{self.server_name}..."})
     
 
     def kill(self, source: CommandSource, context: CommandContext):
         self._execute(
             source,
             "stop",
-            {-1: f"{SERVER_NAME}状态未知, 请回报管理员",
-             0: f"{SERVER_NAME}已停止"},
-            {1: f"强制终止{SERVER_NAME}...",
-             2: f"强制终止{SERVER_NAME}...",
-             3: f"强制终止{SERVER_NAME}..."})
+            {-1: f"{self.server_name}状态未知, 请回报管理员",
+             0: f"{self.server_name}已停止"},
+            {1: f"强制终止{self.server_name}...",
+             2: f"强制终止{self.server_name}...",
+             3: f"强制终止{self.server_name}..."})
 
 
     @new_thread(f"{TITLE}-sync")
@@ -131,17 +182,17 @@ class MirrorManager:
         status_code = self.mcsm_api.status()
         if status_code != 0:
             source.reply({
-                -1: f"{REPLY_TITLE} §c失败 / {SERVER_NAME}状态未知, 请回报管理员",
-                1: f"{REPLY_TITLE} §c失败 / {SERVER_NAME}正在停止, 请等待{SERVER_NAME}停止后进行同步",
-                2: f"{REPLY_TITLE} §c失败 / {SERVER_NAME}正在启动, 请等待{SERVER_NAME}启动后重新停止并进行同步",
-                3: f"{REPLY_TITLE} §c失败 / {SERVER_NAME}正在运行, 请先停止{SERVER_NAME}再进行同步"}[status_code]
+                -1: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}状态未知, 请回报管理员",
+                1: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在停止, 请等待{self.server_name}停止后进行同步",
+                2: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在启动, 请等待{self.server_name}启动后重新停止并进行同步",
+                3: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在运行, 请先停止{self.server_name}再进行同步"}[status_code]
             )
             return
         if self.sync_flag:
-            source.reply(f"{REPLY_TITLE} §c已有正在进行中的同步任务")
+            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c已有正在进行中的同步任务")
         self.sync_flag = True
         try:
-            self.broadcast(f"{REPLY_TITLE} §a正在进行同步...")
+            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§a正在进行同步...")
             t = time.time()
 
             # save the world
@@ -156,19 +207,20 @@ class MirrorManager:
                 self.server.execute(sync_server_config["commands"]["auto_save_on"])
 
             # sync
-            changed_files_count = self.world_sync.sync()
+            changed_files_count, paths_notfound = self.world_sync.sync()
 
             # calc time
             m, s = divmod(time.time()-t, 60)
             h, m = divmod(m, 60)
             t = ("%02d:"%h if h else "") + ("%02d:"%m if m else "") + ("%02d"%s if m or h else ("%.02f"%s).zfill(5))
-            self.broadcast(f"{REPLY_TITLE} §a同步完成! §f用时 §b{t}s §7/ §f更新了§b{changed_files_count}§f个文件")
+            if paths_notfound:
+                self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§c以下文件目录不存在, 已跳过: §f`§7{'§f`§7'.join(paths_notfound)}§f`")
+            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§a同步完成! §f用时 §b{t}s §7/ §f更新了§b{changed_files_count}§f个文件" if changed_files_count != 0 else f"{REPLY_TITLE} §a文件完全相同, 无需同步")
         except Exception as e:
-            self.broadcast(f"{REPLY_TITLE} §c同步时发生错误, 请回报管理员: {e}")
+            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§c同步时发生错误, 请回报管理员: {e}")
         self.sync_flag = False
     
 
     def on_info(self, server: PluginServerInterface, info: Info):
-        if not self.save_world_wait.is_set() and info.is_from_server and re.match(self.config["server"]["saved_world_regex"], info.content):
+        if self.config and not self.save_world_wait.is_set() and info.is_from_server and re.match(self.config["server"]["saved_world_regex"], info.content):
             self.save_world_wait.set() # stop waiting in sync function
-    
