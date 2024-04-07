7ff8ad844c80a93d7431549e2b6744bd1eecd3cc
tanh丶桁 <100672377+tanhHeng@users.noreply.github.com>
2024-04-07 19:37:19 +0800

    Add files via upload

--- mirror_mcsmcdr/mirror_manager.py
+++ mirror_mcsmcdr/mirror_manager.py
@@ -2,6 +2,7 @@ from mcdreforged.api.all import PluginServerInterface, CommandSource, CommandCon
 from mirror_mcsmcdr.constants import DEFAULT_CONFIG, REPLY_TITLE, TITLE, VERSION
 from mirror_mcsmcdr.mcsm_api import MCSManagerApi, MCSManagerApiError
 from mirror_mcsmcdr.file_operation import WorldSync
+from typing import Callable
 from threading import Event
 from copy import deepcopy
 import time, json, re
@@ -22,54 +23,73 @@ def catch_api_error(func):
 class MultiMirrorManager: # The manager at large which manage multi single mirror server manager
 
     def __init__(self, server: PluginServerInterface) -> None:
-        try:
-            config = server.load_config_simple()
-        except:
-            config = server.load_config_simple(default_config=DEFAULT_CONFIG)
+        self.server = server
+        config, default_conf = self.load_config_all()
 
-        default_conf = config[list(config.keys())[0]]
         self.managers = {}
-        for tag, single_conf in config.items():
-            server.logger.info(f"{tag}正在注册")
-            single_conf = self._conf_update(default_conf, single_conf)
-            single_manager = MirrorManager(server, single_conf, tag)
-            server.register_event_listener('mcdr.general_info', single_manager.on_info)
-            self.managers[tag] = single_manager
+
+        succeed, failed = [], []
+        for command_prefix, single_conf in config.items():
+            try:
+                single_conf = self._conf_update(default_conf, single_conf)
+                single_manager = MirrorManager(server, single_conf, command_prefix, reload_method=self.reload_config)
+                server.register_event_listener('mcdr.general_info', single_manager.on_info)
+                self.managers[command_prefix] = single_manager
+                succeed.append(command_prefix)
+            except Exception as e:
+                server.logger.error(f"设置{command_prefix}时出错: {e}")
+                failed.append(command_prefix)
+        server.logger.info(f"{', '.join(succeed)} 构建成功")
+        server.say(f"{REPLY_TITLE} {' '.join(succeed)} §a构建成功{' §7/§c '+' '.join(failed)+' 构建失败' if failed else ''}")
     
 
     def _conf_update(self, default_conf: dict, new_conf: dict):
         default_conf = deepcopy(default_conf)
         for key, value in new_conf.items():
+            if key not in default_conf.keys():
+                continue
             if type(value) == dict:
                 default_conf[key] =  self._conf_update(default_conf[key], new_conf[key])
             elif default_conf[key] != value:
                 default_conf[key] = value
         return default_conf
+    
+
+    def load_config_all(self):
+        try:
+            config = self.server.load_config_simple()
+        except:
+            config = self.server.load_config_simple(default_config=DEFAULT_CONFIG)
+        default_prefix = list(config.keys())[0]
+        default_conf = self._conf_update(DEFAULT_CONFIG["!!mirror"], config[default_prefix])
+        if config[default_prefix] != default_conf:
+            config[default_prefix] = default_conf
+            self.server.save_config_simple(config)
+        return config, default_conf
+
+
+    def reload_config(self, prefix):
+        manager: MirrorManager = self.managers[prefix]
+        config, default_conf = self.load_config_all()
+        if prefix not in config.keys():
+            manager.config_error()
+            return False
+        single_config = self._conf_update(default_conf, config[prefix])
+        manager.reload_config(single_config)
         
         
 class MirrorManager: # The single mirror server manager which manage a specific mirror server
 
 
-    def __init__(self, server: PluginServerInterface, config: dict, tag="default") -> None:
+    def __init__(self, server: PluginServerInterface, config: dict, command_prefix: str, reload_method: Callable) -> None:
         self.server = server
 
-        server.logger.info(f"{tag}已加载")
-        command_prefix = tag
-        # check config
-        self.config = config
-        if None in self.config["mcsm"].values():
-            server.logger.warn(f"请设置配置文件({command_prefix})")
-            server.say(f"§c请设置配置文件§7({command_prefix})")
-            self.config = None
-            return
-        
-        # register api and file operation method
-        self.mcsm_api = MCSManagerApi(**self.config["mcsm"])
-        self.world_sync = WorldSync(**self.config["sync"])
+        server.logger.info(f"{command_prefix}已加载")
+        self.command_prefix = command_prefix
 
-        # set constants
-        self.permission = self.config["command"]["permission"]
-        self.server_name = self.config["display"]["server_name"]
+        # check config
+        if not self.set_config(config):
+            return
 
         # init flag
         self.sync_flag = False
@@ -90,7 +110,9 @@ class MirrorManager: # The single mirror server manager which manage a specific 
             {"text":f"§7{command_prefix} kill §f","clickEvent":{"action":"run_command","value":f"{command_prefix} kill"}},
             {"text":f"强制终止{self.server_name}\n"},
             {"text":f"§7{command_prefix} sync §f","clickEvent":{"action":"run_command","value":f"{command_prefix} sync"}},
-            {"text":f"同步{self.server_name}"},
+            {"text":f"同步{self.server_name}\n"},
+            {"text":f"§7{command_prefix} reload §f","clickEvent":{"action":"run_command","value":f"{command_prefix} sync"}},
+            {"text":f"重载{self.server_name}({command_prefix})的配置文件"},
         ]
 
         # register mcdr command
@@ -103,7 +125,35 @@ class MirrorManager: # The single mirror server manager which manage a specific 
         builder.command(f"{command_prefix} kill", self.kill)
         builder.command(f"{command_prefix} sync", self.sync)
         builder.command(f"{command_prefix} sync", self.sync)
+        builder.command(f"{command_prefix} reload", lambda source, context: reload_method(command_prefix))
         builder.register(server)
+    
+
+    def set_config(self, config):
+        if None in config["mcsm"].values():
+            self.server.logger.warn(f"请设置配置文件({self.command_prefix})")
+            self.server.say(f"§c请设置配置文件§7({self.command_prefix})")
+            self.config = None
+            return False
+        try:
+            self.config, self.mcsm_api, self.world_sync, self.permission, self.server_name = config, MCSManagerApi(**config["mcsm"]), \
+                WorldSync(**config["sync"]), config["command"]["permission"], config["display"]["server_name"]
+            return True
+        except:
+            self.config_error()
+            return False
+    
+
+    def reload_config(self, config):
+        if not self.set_config(config):
+            self.config_error()
+            return
+        self.broadcast(f"{REPLY_TITLE} §a配置文件重载成功§7({self.command_prefix})")
+
+
+    def config_error(self):
+        self.server.logger.warn(f"无效的配置文件({self.command_prefix})")
+        self.server.say(f"{REPLY_TITLE} §c无效的配置文件§7({self.command_prefix})")
 
 
     def broadcast(self, text):
@@ -118,81 +168,107 @@ class MirrorManager: # The single mirror server manager which manage a specific 
             self.server.logger.info("\n"+"".join([i["text"] for i in self.help_msg]))
 
 
+    def check_permission(self, source: CommandSource, command: str):
+        if not source.has_permission_higher_than(self.permission[command]):
+            source.reply(f"{REPLY_TITLE} §b{self.server_name}§c操作权限不足")
+            return False
+        return True
+
+
     @catch_api_error
     def _execute(self, source: CommandSource, command: str, failed_prompt: dict, succeeded_prompt: dict): # <failed_prompt> & <succeeded_prompt> : {status_code: "prompt"}
-        if not source.has_permission_higher_than(self.permission[command]):
-            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c权限不足")
-            return
+        if not self.check_permission(source, command): return
         status_code = self.mcsm_api.status()
         if status_code in failed_prompt.keys():
-            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c失败: "+failed_prompt[status_code])
+            source.reply(f"{REPLY_TITLE} §c操作失败: §b{self.server_name}§f"+failed_prompt[status_code])
+            return False
         elif status_code in succeeded_prompt.keys():
             rep = eval(f"self.mcsm_api.{command}()")
             self.server.logger.info(rep)
-            self.broadcast(f"{REPLY_TITLE} §a"+succeeded_prompt[status_code])
+            self.broadcast(f"{REPLY_TITLE} §b{self.server_name}§a"+succeeded_prompt[status_code])
+            return True
 
         
     @catch_api_error
     def status(self, source: CommandSource, context: CommandContext):
-        if not source.has_permission_higher_than(self.permission["status"]):
-            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c权限不足")
-            return
-        source.reply(f"{REPLY_TITLE} §7({self.server_name})§b{self.server_name}§f{self.mcsm_api.status_to_text[self.mcsm_api.status()]}")
+        if not self.check_permission(source, "status"): return
+        source.reply(f"{REPLY_TITLE} §b{self.server_name}§f{self.mcsm_api.status_to_text[self.mcsm_api.status()]}")
+        return True
 
 
     def start(self, source: CommandSource, context: CommandContext):
-        self._execute(
+        return self._execute(
             source,
             "start",
-            {-1: f"{self.server_name}状态未知, 请回报管理员",
-             1: f"{self.server_name}正在停止, 请等待{self.server_name}停止后重新启动",
-             2: f"{self.server_name}正在启动, 请稍等",
-             3: f"{self.server_name}正在运行"},
-            {0: f"正在启动{self.server_name}..."})
+            {-1: f"状态未知, 请回报管理员",
+             1: f"正在停止, 请等待{self.server_name}停止后重新启动",
+             2: f"正在启动, 请稍等",
+             3: f"正在运行"},
+            {0: f"正在启动..."})
     
 
     def stop(self, source: CommandSource, context: CommandContext):
-        self._execute(
+        return self._execute(
             source,
             "stop",
-            {-1: f"{self.server_name}状态未知, 请回报管理员",
-             0: f"{self.server_name}已停止",
-             1: f"{self.server_name}正在停止, 请稍等",
-             2: f"{self.server_name}正在启动, 请等待{self.server_name}启动后重新停止",},
-            {3: f"正在关闭{self.server_name}..."})
+            {-1: f"状态未知, 请回报管理员",
+             0: f"已停止",
+             1: f"正在停止, 请稍等",
+             2: f"正在启动, 请等待§b{self.server_name}§c启动后重新停止",},
+            {3: f"正在关闭..."})
     
 
     def kill(self, source: CommandSource, context: CommandContext):
-        self._execute(
+        return self._execute(
             source,
             "stop",
-            {-1: f"{self.server_name}状态未知, 请回报管理员",
-             0: f"{self.server_name}已停止"},
-            {1: f"强制终止{self.server_name}...",
-             2: f"强制终止{self.server_name}...",
-             3: f"强制终止{self.server_name}..."})
+            {-1: f"状态未知, 请回报管理员",
+             0: f"已停止"},
+            {1: f"强制终止...",
+             2: f"强制终止...",
+             3: f"强制终止..."})
 
 
     @new_thread(f"{TITLE}-sync")
     @catch_api_error
     def sync(self, source: CommandSource, context: CommandContext):
-        if not source.has_permission_higher_than(self.permission["status"]):
-            source.reply(f"{REPLY_TITLE} §c权限不足")
-            return
-        status_code = self.mcsm_api.status()
-        if status_code != 0:
-            source.reply({
-                -1: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}状态未知, 请回报管理员",
-                1: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在停止, 请等待{self.server_name}停止后进行同步",
-                2: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在启动, 请等待{self.server_name}启动后重新停止并进行同步",
-                3: f"{REPLY_TITLE} §7({self.server_name})§c失败 / {self.server_name}正在运行, 请先停止{self.server_name}再进行同步"}[status_code]
-            )
-            return
+        if not self.check_permission(source, "sync"): return
+        
+        auto_restart_flag = False
+        sync_action_config = self.config["command"]["action"]["sync"]
+        if sync_action_config["ensure_server_closed"]:
+            status_code = self.mcsm_api.status()
+            
+            if status_code == 0:
+                pass
+            elif status_code != 3 or not sync_action_config["auto_server_restart"]:
+                source.reply({
+                    -1: f"{REPLY_TITLE} §c失败 / §b{self.server_name}§c状态未知, 请回报管理员",
+                    1: f"{REPLY_TITLE} §c失败 / §b{self.server_name}§c正在停止, 请等待{self.server_name}停止后进行同步",
+                    2: f"{REPLY_TITLE} §c失败 / §b{self.server_name}§c正在启动, 请等待{self.server_name}启动后重新停止并进行同步",
+                    3: f"{REPLY_TITLE} §c失败 / §b{self.server_name}§c正在运行, 请先停止{self.server_name}再进行同步"}[status_code]
+                )
+                return
+            else:
+                source.reply(f"{REPLY_TITLE} §b{self.server_name}§f未停止, 自动关闭{self.server_name}并进行同步...")
+                if not self.stop(source, context):
+                    return
+                interval, times = sync_action_config["check_status_interval"], sync_action_config["max_attempt_times"]
+                for i in range(times):
+                    time.sleep(interval)
+                    status_code = self.mcsm_api.status()
+                    if status_code == 0:
+                        break
+                else:
+                    source.reply(f"{REPLY_TITLE} §c自动关闭失败, 当前§b{self.server_name}§c状态: §a{self.mcsm_api.status_to_text()}")
+                    return
+                auto_restart_flag = True
+
         if self.sync_flag:
-            source.reply(f"{REPLY_TITLE} §7({self.server_name})§c已有正在进行中的同步任务")
+            source.reply(f"{REPLY_TITLE} §b{self.server_name}§c已有正在进行中的同步任务")
         self.sync_flag = True
         try:
-            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§a正在进行同步...")
+            self.broadcast(f"{REPLY_TITLE} §b{self.server_name}§a正在进行同步...")
             t = time.time()
 
             # save the world
@@ -214,11 +290,14 @@ class MirrorManager: # The single mirror server manager which manage a specific 
             h, m = divmod(m, 60)
             t = ("%02d:"%h if h else "") + ("%02d:"%m if m else "") + ("%02d"%s if m or h else ("%.02f"%s).zfill(5))
             if paths_notfound:
-                self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§c以下文件目录不存在, 已跳过: §f`§7{'§f`§7'.join(paths_notfound)}§f`")
-            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§a同步完成! §f用时 §b{t}s §7/ §f更新了§b{changed_files_count}§f个文件" if changed_files_count != 0 else f"{REPLY_TITLE} §a文件完全相同, 无需同步")
+                self.broadcast(f"{REPLY_TITLE} §b{self.server_name}§c的以下文件目录不存在, 已跳过: §f`§7{'§f`§7'.join(paths_notfound)}§f`")
+            self.broadcast(f"{REPLY_TITLE} §b{self.server_name}§a同步完成! §f用时 §b{t}s §7/ §f更新了§b{changed_files_count}§f个文件" if changed_files_count != 0 else f"{REPLY_TITLE} §a文件完全相同, 无需同步")
         except Exception as e:
-            self.broadcast(f"{REPLY_TITLE} §7({self.server_name})§c同步时发生错误, 请回报管理员: {e}")
+            self.broadcast(f"{REPLY_TITLE} §b{self.server_name}§c同步时发生错误, 请回报管理员: {e}")
         self.sync_flag = False
+
+        if auto_restart_flag:
+            self.start(source, context)
     
 
     def on_info(self, server: PluginServerInterface, info: Info):
