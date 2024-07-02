8c11126dec5366c590c9b46c0e4dd783c582bf62
tanh丶桁 <100672377+tanhHeng@users.noreply.github.com>
2024-07-02 13:06:19 +0800

    Update __init__.py

--- mirror_mcsmcdr/__init__.py
+++ mirror_mcsmcdr/__init__.py
@@ -1,6 +1,5 @@
 from mcdreforged.api.all import PluginCommandSource
 from mirror_mcsmcdr.mirror_manager import MultiMirrorManager
-from mirror_mcsmcdr.utils.constants import TITLE
 
 def on_load(server: PluginCommandSource, prev_module):
     manager = MultiMirrorManager(server)
