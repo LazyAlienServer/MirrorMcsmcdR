e3d00754d579b260f822635a09ef4f27930504b4
tanhHeng <2196226582@qq.com>
2024-11-29 23:14:19 +0800

    style: 为HTTPApi使用了AbstractBaseClass

--- mirror_mcsmcdr/utils/api/mcsm_api.py
+++ mirror_mcsmcdr/utils/api/mcsm_api.py
@@ -1,7 +1,49 @@
 import requests, json
 from requests import Response
 from mirror_mcsmcdr.utils.display_utils import rtr
+from abc import ABC, abstractmethod
 
+class HTTPApiError(Exception):
+
+    def __init__(self, req: Response) -> None:
+        super().__init__("HTTPApiError")
+
+class AbstractHTTPApi(ABC):
+
+    def __init__(self, enable: bool, url: str, *args, **kwargs) -> None:
+        self.enable = enable
+        self.url = url if url[-1] != "/" else url[:-1]
+        self.params = kwargs
+        self.status_to_text = {
+            -1: "unknown",
+            0: "stopped",
+            1: "stopping",
+            2: "starting",
+            3: "running"
+        }
+    
+    def _request(self, path: str, exception: HTTPApiError = HTTPApiError):
+        req : Response = requests.get(url=self.url+path, params=self.params)
+        if req.status_code == 200:
+            return json.loads(req.text)
+        else:
+            raise exception(req)
+    
+    @abstractmethod
+    def status(self) -> str:
+        ...
+    
+    @abstractmethod
+    def start(self) -> str:
+        return "success"
+    
+    @abstractmethod
+    def stop(self) -> str:
+        return "success"
+    
+    @abstractmethod
+    def kill(self) -> str:
+        return "success"
 
 class MCSManagerApiError(Exception):
 
@@ -14,30 +56,13 @@ class MCSManagerApiError(Exception):
         super().__init__(rtr(f"mcsm.error.{req.status_code}", title=False, error=error).to_plain_text())
 
 
-class MCSManagerApi:
+class MCSManagerApi(AbstractHTTPApi):
 
     def __init__(self, enable: bool, url: str, uuid: str, remote_uuid: str, apikey: str) -> None:
-        self.enable = enable
-        self.url = url if url[-1] != "/" else url[:-1]
-        self.params = {
-            "uuid": uuid,
-            "remote_uuid": remote_uuid,
-            "apikey": apikey
-        }
-        self.status_to_text = {
-            -1: "unknown",
-            0: "stopped",
-            1: "stopping",
-            2: "starting",
-            3: "running"
-        }
+        super().__init__(enable, url, uuid = uuid, remote_uuid = remote_uuid, apikey = apikey)
     
     def _request(self, path: str):
-        req : Response = requests.get(url=self.url+path, params=self.params)
-        if req.status_code == 200:
-            return json.loads(req.text)
-        else:
-            raise MCSManagerApiError(req)
+        return super()._request(path, MCSManagerApiError)
     
     def status(self):
         return self.status_to_text[self._request("/api/instance")["data"]["status"]]
