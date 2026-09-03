a4ee6fad4815fa24b5efdab259ad1e3f565f897a
tanhHeng <tanh_heng@outlook.com>
2026-09-03 21:14:48 +0800

    style: lint fix

--- mirror_mcsmcdr/config/mirror_config.py
+++ mirror_mcsmcdr/config/mirror_config.py
@@ -1,11 +1,11 @@
+from typing import Dict
 from mcdreforged.api.all import Serializable
 from mirror_mcsmcdr.config.command_config import CommandConfig
 from mirror_mcsmcdr.config.proxy_config import MCSMConfig, TerminalConfig, RConConfig
 from mirror_mcsmcdr.config.synchronize_config import SynchronizationConfig
-from typing import Dict
 
 class DisplayConfig(Serializable):
-    server_name: str = "Mirror"
+    server_name: str = 'Mirror'
 
 class MirrorConfig(Serializable):
     mcsm: MCSMConfig = MCSMConfig()
@@ -16,4 +16,4 @@ class MirrorConfig(Serializable):
     display: DisplayConfig = DisplayConfig()
 
 class MultiMirrorConfig(Dict[str, MirrorConfig]):
-    ...
+    pass
